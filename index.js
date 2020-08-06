e8ac44f75da2ea59540669266c56e156e02968da
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-06 22:55:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function add(x,y){
   return x+y
 }
+
+function subtract(x,y){
+  return x-y
+}
+
+function multiply(x,y){
+  return x*y
+}
+
+function divide(x,y){
+  return x/y
+}
+
+function increment(x){
+  return x++
+}
